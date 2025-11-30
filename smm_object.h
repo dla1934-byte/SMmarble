e99d519f1962b9ce9c633fd8501690bb176ac49c
dla1934-byte <dla1934@sookmyung.ac.kr>
2025-11-30 18:51:26 +0900

    2st

--- smm_object.h
+++ smm_object.h
@@ -34,10 +34,11 @@
 
 
 //object generation
-int smmObj_genNode(char *name, int type, int credit, int energy);
+int smmObj_getNode(char *name, int type, int credit, int energy);
 char* smmObj_getName(int node_nr);
 int smmObj_getType(int node_nr);
-char* smmObj_getTypeName(int node_nr);
+int smmObj_getEnergy(int node_nr);
+char* smmObj_getNodeName(int node_type);
 //member retrieving
 
 
